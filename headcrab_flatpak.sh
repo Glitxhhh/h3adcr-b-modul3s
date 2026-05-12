21026aa0786ab7ae81408e538b5ba44416426b39
_drazy <48094316+Deadboy666@users.noreply.github.com>
2026-05-12 15:10:00 -0500

    Update headcrab_flatpak.sh

--- headcrab_flatpak.sh
+++ headcrab_flatpak.sh
@@ -2,7 +2,7 @@
 
 
     STEAM_CLIENT="$HOME/.var/app/com.valvesoftware.Steam/.local/share/Steam/client.sh"
-    INJECT_SLS="LD_AUDIT=$HOME/.var/app/com.valvesoftware.Steam/.local/share/SLSsteam/library-inject.so:$HOME/.var/app/com.valvesoftware.Steam/.local/share/SLSsteam/SLSsteam.so"
+    INJECT_SLS="LD_AUDIT=$HOME/.var/app/com.valvesoftware.Steam/.local/share/SLSsteam/library-inject.so:$HOME/.var/app/com.valvesoftware.Steam/.local/share/SLSsteam/SLSsteam.so:$HOME/.var/app/com.valvesoftware.Steam/.local/share/CloudRedirect/cloud_redirect.so"
 
        GameLauncher(){
         export $INJECT_SLS &> /dev/null
