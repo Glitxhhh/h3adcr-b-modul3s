8d6edf4191c90a5e0b9f81c5e0cbf14c6f1ee63f
_drazy <48094316+Deadboy666@users.noreply.github.com>
2026-05-21 13:42:55 -0500

    Update headcrab_native.sh

--- headcrab_native.sh
+++ headcrab_native.sh
@@ -2,10 +2,11 @@
 
 
     STEAM_CLIENT="$HOME/.steam/steam/client.sh"
-    INJECT_SLS="LD_AUDIT=$HOME/.local/share/SLSsteam/library-inject.so:$HOME/.local/share/SLSsteam/SLSsteam.so:$HOME/.local/share/CloudRedirect/cloud_redirect.so"
-
+    INJECT_SLS="LD_AUDIT=$HOME/.local/share/SLSsteam/library-inject.so:$HOME/.local/share/SLSsteam/SLSsteam.so"
+    INJECT_CR="LD_PRELOAD=$HOME/.local/share/CloudRedirect/cloud_redirect.so"
        GameLauncher(){
         export $INJECT_SLS &> /dev/null
+        export $INJECT_CR &> /dev/null
         source $STEAM_CLIENT "$@"
         }
 
